f3c11e3bfadb16548068d2df2cfedfe1580bd799
YUSUKE TANABE <e12707@kmjp.local>
2019-06-03 17:31:14 +0900

    修正

--- reservasion-app/src/views/Home.vue
+++ reservasion-app/src/views/Home.vue
@@ -7,17 +7,15 @@
             <th>{{selectedDate.year}}</th>
           </tr>
           <tr>
-            <th><div v-on:click="moveLastMonth">＜</div></th>
-            <th>{{selectedDate.month}}</th>
-            <th><div v-on:click="moveNextMonth">＞</div></th>
+            <th v-for="dateInfo in calendar" v-bind:key="dateInfo.date">{{dateInfo.date}}</th>
           </tr>
           <tr>
-            <th v-for="week in weeks" v-bind:key="week">{{week}}</th>
+            <th v-for="dateInfo in calendar" v-bind:key="dateInfo.date">{{dateInfo.day}}</th>
           </tr>
         </thead>
         <tbody class="calendar-body">
-          <tr v-for="weekCalenders in calendars" v-bind:key="weekCalenders">
-            <td v-for="day in weekCalenders" v-bind:key="day">{{day}}</td>
+          <tr v-for="scheduleByTime in schedule" v-bind:key="scheduleByTime.time">
+            <td v-for="dateInfo in scheduleByTime.calendar" v-bind:key="dateInfo.date">{{dateInfo.value}}</td>
           </tr>
         </tbody>
       </tabel>
@@ -33,7 +31,6 @@ export default {
   components: {},
   data() {
     return {
-      weeks: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
       selectedDate: { year: 0, month: 0, date: 0, day: 0 }
     };
   },
@@ -44,7 +41,6 @@ export default {
     this.selectedDate.month = date.getMonth() + 1;
     this.selectedDate.date = date.getDate();
     this.selectedDate.day = date.getDay();
-
 
     // セットされている月の初日の曜日を取得
     const firstDayOfWeek = new Date(
@@ -59,22 +55,20 @@ export default {
       0
     ).getDate();
 
-    const weekCalendars = [];
-    let indexDate = this.selectedDate.date + 1;
-    let indexDay = this.selectedDate.day + 1;
+    /*
+    const weeklyCalendar = [];
 
     for(let day = 0; day < 7; day++){
       const dateInfo = {};
       //初日に曜日に達するまで、または、最終日を過ぎたときは空の文字列を格納
       if(indexDate <= lastDay) {
-        dateInfo.date = indexDate;
+        dateInfo.date = this.selectedDate.month + '/' + indexDate;
       }
       //
       else{
-        this.moveNextMonth();
         indexDate = 1;
-        dateInfo.date = indexDate;
-        dateInfo.month = this.getMonthName(this.selectedDate.month);
+        this.selectedDate.month++;
+        dateInfo.date = this.selectedDate.month + '/' + indexDate;
       }
 
       if(indexDay < 7){
@@ -84,11 +78,64 @@ export default {
         indexDay = 0;
         dateInfo.day = this.getDayName(indexDay);
       }
-      weekCalendars.push(dateInfo);
+      weeklyCalendar.push(dateInfo);
       indexDate++;
       indexDay++;
     }
-    console.log(weekCalendars)
+    */
+
+    let time = 14;
+    const calendar = [];
+
+    while(time < 23){
+      const weeklyCalendar = [];
+      let indexDate = this.selectedDate.date;
+      let indexDay = this.selectedDate.day;
+
+      for(let day = 0; day < 7; day++){
+        const dateInfo = {};
+        //日付の設定
+        if(indexDate <= lastDay) {
+          dateInfo.date = this.selectedDate.month + '/' + indexDate;
+        }
+        //
+        else{
+          indexDate = 1;
+          this.selectedDate.month++;
+          dateInfo.date = this.selectedDate.month + '/' + indexDate;
+        }
+
+        //曜日の設定
+        if(indexDay < 7){
+          dateInfo.day = this.getDayName(indexDay);
+        }
+        else{
+          indexDay = 0;
+          dateInfo.day = this.getDayName(indexDay);
+        }
+
+        //時間の設定
+        dateInfo.hour = time + ':00';
+
+        //値の設定
+        if((dateInfo.day === '土' && time >= 19) || (dateInfo.day !== '日' && time >= 21)){
+          //仮の値
+          dateInfo.value = '問';
+        }
+        else{
+          dateInfo.value = '要';
+        }
+
+        weeklyCalendar.push(dateInfo);
+        indexDate++;
+        indexDay++;
+      }
+
+      calendar.push({time: time, calendar: weeklyCalendar});
+      time++;
+    }
+
+    console.log(calendar);
 
   },
   methods: {
@@ -116,36 +163,12 @@ export default {
     //指定された曜日の名称を取得する関数
     //---------------------
     getDayName(day) {
-      const monthName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      const monthName = ["日", "月", "火", "水", "木", "金", "土"];
       return monthName[day];
-    },
-    //---------------------
-    //ひと月前へ戻す関数
-    //---------------------
-    moveLastMonth() {
-      //1月の場合は12月へ変更し、昨年へ
-      if (this.selectedDate.month === 1) {
-        this.selectedDate.year--;
-        this.selectedDate.month = 12;
-      } else {
-        this.selectedDate.month--;
-      }
-    },
-    //---------------------
-    //ひと月後へ進める関数
-    //---------------------
-    moveNextMonth() {
-      //12月の場合は1月へ変更し、翌年へ
-      if (this.selectedDate.month === 12) {
-        this.selectedDate.year++;
-        this.selectedDate.month = 1;
-      } else {
-        this.selectedDate.month++;
-      }
     }
   },
   computed: {
-    calendars() {
+    calendar() {
       // セットされている月の初日の曜日を取得
       const firstDayOfWeek = new Date(
         this.selectedDate.year,
@@ -158,29 +181,103 @@ export default {
         this.selectedDate.month,
         0
       ).getDate();
-      
-      const calendars = [];
-      let indexDay = 1;
-      let fisrtRoopFlg = true; //初回のループの時のみ立つフラグ
 
-      //カレンダー生成処理
-      while(indexDay <= lastDay) {
-        const weeksCalendars = [];
-        for(let dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++){
-          //初日に曜日に達するまで、または、最終日を過ぎたときは空の文字列を格納
-          if((fisrtRoopFlg && firstDayOfWeek > dayOfWeek) || indexDay > lastDay) {
-            weeksCalendars.push('');
-          }
-          //それ以外は日にちを格納
-          else{
-            weeksCalendars.push(indexDay);
-            indexDay++;
-            fisrtRoopFlg = false;
-          }
+      const calendar = [];
+      let indexDate = this.selectedDate.date;
+      let indexDay = this.selectedDate.day;
+
+      for(let day = 0; day < 7; day++){
+        const dateInfo = {};
+        //初日に曜日に達するまで、または、最終日を過ぎたときは空の文字列を格納
+        if(indexDate <= lastDay) {
+          dateInfo.date = this.selectedDate.month + '/' + indexDate;
         }
-        calendars.push(weeksCalendars);
+        //
+        else{
+          indexDate = 1;
+          this.selectedDate.month++;
+          dateInfo.date = this.selectedDate.month + '/' + indexDate;
+        }
+
+        if(indexDay < 7){
+          dateInfo.day = this.getDayName(indexDay);
+        }
+        else{
+          indexDay = 0;
+          dateInfo.day = this.getDayName(indexDay);
+        }
+        calendar.push(dateInfo);
+        indexDate++;
+        indexDay++;
       }
-      return calendars;
+      
+      return calendar;
+    },
+    schedule() {
+      // セットされている月の初日の曜日を取得
+      const firstDayOfWeek = new Date(
+        this.selectedDate.year,
+        this.selectedDate.month - 1
+      ).getDay();
+
+      // セットされている月の晦日を取得
+      const lastDay = new Date(
+        this.selectedDate.year,
+        this.selectedDate.month,
+        0
+      ).getDate();
+
+      let time = 14;
+      const calendar = [];
+
+      while(time < 23){
+        const weeklyCalendar = [];
+        let indexDate = this.selectedDate.date;
+        let indexDay = this.selectedDate.day;
+
+        for(let day = 0; day < 7; day++){
+          const dateInfo = {};
+          //日付の設定
+          if(indexDate <= lastDay) {
+            dateInfo.date = this.selectedDate.month + '/' + indexDate;
+          }
+          //
+          else{
+            indexDate = 1;
+            this.selectedDate.month++;
+            dateInfo.date = this.selectedDate.month + '/' + indexDate;
+          }
+
+          //曜日の設定
+          if(indexDay < 7){
+            dateInfo.day = this.getDayName(indexDay);
+          }
+          else{
+            indexDay = 0;
+            dateInfo.day = this.getDayName(indexDay);
+          }
+
+          //時間の設定
+          dateInfo.hour = time + ':00';
+
+          //値の設定
+          if((dateInfo.day === '土' && time >= 19) || (dateInfo.day !== '日' && time >= 21)){
+            //仮の値
+            dateInfo.value = '問';
+          }
+          else{
+            dateInfo.value = '要';
+          }
+
+          weeklyCalendar.push(dateInfo);
+          indexDate++;
+          indexDay++;
+        }
+
+        calendar.push({time: time, calendar: weeklyCalendar});
+        time++;
+      }
+      return calendar;
     }
   }
 };
